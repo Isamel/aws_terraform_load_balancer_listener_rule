54f589ddb7b29d5c422e2e27eaf7bd619b1b28e6
unknown <Jose Salinas>
2020-06-13 19:20:23 -0700

    add main

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "null_resource" "depends_on" {
+    triggers = {
+        depends_on = join("", var.alb_listener_rule_depends_on)
+    }
+}
+
+resource "aws_lb_listener_rule" "alb_listener_rule_rule_terraform" {
+    count                = var.alb_listener_rule_count
+    depends_on           = [null_resource.depends_on]
+    listener_arn         = var.alb_listener_rule_listener_arn
+    condition {
+        field            = var.alb_listener_rule_condition
+        values           = var.alb_listener_rule_condition
+    }
+    action {
+        type             = var.alb_listener_rule_action_type
+        redirect {
+            port         = var.alb_listener_rule_action_redirect_port
+            protocol     = var.alb_listener_rule_action_redirect_protocol
+            status_code  = var.alb_listener_rule_action_redirect_status_code
+        }
+        target_group_arn = var.alb_listener_rule_action_target_group_arn
+    }
+}
